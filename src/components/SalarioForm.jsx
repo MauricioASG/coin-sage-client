364e07d3f7767bdd38990b52119561e7d4572172
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2024-05-31 11:46:53 -0600

    Show user salary if already registered

--- src/components/SalarioForm.jsx
+++ src/components/SalarioForm.jsx
@@ -1,9 +1,25 @@
 // src/components/SalarioForm.jsx
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const SalarioForm = ({ userId }) => {
   const [salario, setSalario] = useState('');
+
+  useEffect(() => {
+    const fetchSalario = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3050/usuarios/${userId}/salario`);
+        if (response.data && response.data.salario) {
+          setSalario(response.data.salario);
+        }
+      } catch (error) {
+        console.error('Error al obtener el salario:', error);
+      }
+    };
+
+    fetchSalario();
+  }, [userId]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,8 +38,9 @@ const SalarioForm = ({ userId }) => {
         value={salario}
         onChange={(e) => setSalario(e.target.value)}
         placeholder="Ingresa tu salario mensual"
+        className="textInput"
       />
-      <button type="submit">Registrar Salario</button>
+      <button type="submit" className="submit-button">Registrar Salario</button>
     </form>
   );
 };
